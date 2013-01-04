178249681f7d8ab87e22c0b2d01e2bb54de0fdc9
Ján Jakabovic <janyjakabovic@gmail.com>
2013-01-04 22:39:43 +0100

    upload

---
+++ Unit1.dfm
@@ -0,0 +1,66 @@
+object Form1: TForm1
+  Left = 192
+  Top = 114
+  Width = 696
+  Height = 480
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 248
+    Top = 16
+    Width = 55
+    Height = 13
+    Caption = 'Platn'#233' '#269#237'sla'
+  end
+  object Label2: TLabel
+    Left = 472
+    Top = 16
+    Width = 62
+    Height = 13
+    Caption = 'Zadan'#233' '#269#237'sla'
+  end
+  object Button1: TButton
+    Left = 232
+    Top = 192
+    Width = 75
+    Height = 25
+    Caption = 'OK'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Edit1: TEdit
+    Left = 208
+    Top = 136
+    Width = 121
+    Height = 21
+    TabOrder = 1
+    Text = 'Edit1'
+  end
+  object Memo1: TMemo
+    Left = 392
+    Top = 40
+    Width = 185
+    Height = 369
+    Lines.Strings = (
+      'Memo1')
+    TabOrder = 2
+  end
+  object Memo2: TMemo
+    Left = 192
+    Top = 40
+    Width = 185
+    Height = 369
+    Lines.Strings = (
+      'Memo2')
+    TabOrder = 3
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,72 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Memo1: TMemo;
+    Memo2: TMemo;
+    Label1: TLabel;
+    Label2: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  n:boolean; a:integer;
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var d,i,j:integer;  c:string;
+begin
+
+if n=false then begin
+                a:=strtoint(edit1.text);
+                n:=true;
+                end
+            else begin
+              memo1.Lines.Add(edit1.text);
+              if memo1.Lines.count=a then begin
+                                        Edit1.Visible:=false;
+                                        button1.Visible:=false;
+                                        memo2.Visible:=true;
+                                        for i:=0 to (a-1) do begin
+                                                          d:=0;
+                                                          c:=memo1.Lines.Strings[i];
+                                                          for j:=1 to length(c) do
+                                                                      if (c[j]<='3') and (c[j]>='0') then d:=d+1;
+                                                          if d=length(c) then memo2.Lines.add(c);
+                                                            end;
+                                            end;
+                end;
+if memo2.Visible=true then begin label1.visible:=true;
+                            label2.visible:=true;
+                            end;
+end;
+
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+label1.Visible:=false;
+label2.Visible:=false;
+n:=false;
+edit1.Text:='Zadaj èíslo.';
+memo1.Clear;
+memo2.Clear;
+memo2.Visible:=false;
+end;
+
+end.
